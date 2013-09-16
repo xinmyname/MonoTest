34d46847320055e2ea6d5092d6f2794cb5250b2c
Andy Sherwood <sherwooda@cleanwaterservices.org>
2013-09-16 09:01:23 -0700

    Dynamically load Sqlite assembly based on platform (Mono)

--- Infrastructure/DatabaseFactory.cs
+++ Infrastructure/DatabaseFactory.cs
@@ -1,30 +1,44 @@
 using System;
 using System.Data;
-using Community.CsharpSqlite.SQLiteClient;
+//using Community.CsharpSqlite.SQLiteClient;
+using System.Reflection;
+using System.Linq;
 
 namespace MonoTest.Infrastructure
 {
     public class DatabaseFactory
     {
         private readonly string _dbPath;
+		private readonly Assembly _sqliteAssembly;
+		private readonly string _conStr;
+		private readonly Type _sqliteConnectionType;
 
         public DatabaseFactory(SettingsStore settingsStore)
         {
             _dbPath = settingsStore.Load().DatabasePath;
-        }
+
+			if (Environment.OSVersion.Platform == PlatformID.Unix)
+				_sqliteAssembly = Assembly.Load("Mono.Data.Sqlite");
+			else
+				_sqliteAssembly = Assembly.Load("Community.CsharpSqlite.SQLiteClient");
+
+			Type[] exportedTypes = _sqliteAssembly.GetExportedTypes();
+
+			Type sqliteConnectionStringBuilderType = exportedTypes.Where(t => t.Name == "SqliteConnectionStringBuilder").Single();
+			_sqliteConnectionType = exportedTypes.Where(t => t.Name == "SqliteConnection").Single();
+
+			object conBuilder = Activator.CreateInstance(sqliteConnectionStringBuilderType);
+			sqliteConnectionStringBuilderType.GetProperty("Uri").SetValue(conBuilder, new Uri(_dbPath).AbsoluteUri, null);
+			_conStr = (string)sqliteConnectionStringBuilderType.GetProperty("ConnectionString").GetValue(conBuilder, null);
+		}
 
         public IDbConnection OpenDatabase()
         {
-            var conBuilder = new SqliteConnectionStringBuilder 
-                {
-                    Uri = new Uri(_dbPath).AbsoluteUri
-                };
+			var con = (IDbConnection)Activator.CreateInstance(_sqliteConnectionType);
+			con.ConnectionString = _conStr;
+			con.Open();
 
-            string conStr = conBuilder.ConnectionString;
-
-            IDbConnection con = new SqliteConnection(conStr);
-            con.Open();
-            return con;
+			return con;
         }
     }
 }
